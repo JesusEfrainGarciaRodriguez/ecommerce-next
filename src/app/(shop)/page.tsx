8c53dca7d5cedc8609f2b7ca80fee5adb3e1250d
Efrain Garcia <efraingarcia_86@hotmail.com>
2026-03-22 20:42:26 -0700

    Add base routes

---
+++ src/app/(shop)/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Shop() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center font-title">
+      <h1>Shop</h1>
+    </div>
+  );
+}
---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Login() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center font-title">
+      <h1>Login page</h1>
+    </div>
+  );
+}
