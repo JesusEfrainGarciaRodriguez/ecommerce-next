8ee52c45d0599a3ecb93def3756e6cb08074c4bf
Efrain Garcia <efraingarcia_86@hotmail.com>
2026-03-23 13:38:58 -0700

    Add category page logic

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,17 +1,33 @@
+import { ProductGrid, Title } from "@/components";
 import { notFound } from "next/navigation";
+import { initialData } from "@/seed/seed";
+import { Category } from "@/interfaces";
 
 interface Props {
-  params: Promise<{ id: string }>;
+  params: Promise<{ id: Category }>;
 }
+
+const seedProducts = initialData.products;
 
 export default async function CategoryPage({ params }: Props) {
   const { id } = await params;
 
-  if (id === "kids") return notFound();
+  const products = seedProducts.filter((product) => product.gender === id);
+
+  const labels: Record<Category, string> = {
+    men: "Hombre",
+    women: "Mujer",
+    kid: "Niño",
+    unisex: "Unisex",
+  };
+
+  if (products.length === 0) return notFound();
 
   return (
-    <div>
-      <h1>Category Page {id}</h1>
-    </div>
+    <>
+      <Title title={labels[id]} subtitle="Todos los productos" className="mb-2"/>
+
+      <ProductGrid products={products} />  
+    </>
   );
 }
