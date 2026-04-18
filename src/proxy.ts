ecc90d9294b536546637d73c5162643c32bbef8a
Efrain Garcia <efraingarcia_86@hotmail.com>
2026-04-18 11:59:51 -0700

    Create proxy to protected routes

---
+++ src/proxy.ts
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getSession } from "./lib/get-session";
+
+export async function proxy(request: NextRequest) {
+  const session = await getSession();
+
+  if (!session) {
+    return NextResponse.redirect(new URL("auth/login", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/profile"],
+};
