c73ae25ab57fb7462a83b4006056cfa54b9f91bf
Efrain Garcia <efraingarcia_86@hotmail.com>
2026-05-11 23:36:14 -0700

    Refactor PaypalButton component to handle loading state and improve user feedback

--- src/components/paypal/PaypalButton.tsx
+++ src/components/paypal/PaypalButton.tsx
@@ -1,29 +1,43 @@
 "use client";
 
 import {
+  INSTANCE_LOADING_STATE,
   OnApproveDataOneTimePayments,
   PayPalOneTimePaymentButton,
+  usePayPal,
 } from "@paypal/react-paypal-js/sdk-v6";
 
 export const PaypalButton = () => {
+  const { loadingStatus } = usePayPal();
+
+  const isPending = loadingStatus === INSTANCE_LOADING_STATE.PENDING;
+
+  if (isPending) {
+    return (
+      <p className="animated-pulse antialiased text-lg mb-1 bg-gray-200 rounded">
+        &nbsp;
+      </p>
+    );
+  }
+
   return (
     <PayPalOneTimePaymentButton
-          presentationMode="auto"
-          /* TODO: Implement createOrder function */
-          createOrder={async () => {
-            const response = await fetch("/api/create-order", {
-              method: "POST",
-            });
-            const { orderId } = await response.json();
-            return { orderId };
-          }}
-          /* TODO: Implement onApprove function */
-          onApprove={async ({ orderId }: OnApproveDataOneTimePayments) => {
-            await fetch(`/api/capture-order/${orderId}`, {
-              method: "POST",
-            });
-            console.log("Payment captured!");
-          }}
-        />
-    );
+      presentationMode="auto"
+      /* TODO: Implement createOrder function */
+      createOrder={async () => {
+        const response = await fetch("/api/create-order", {
+          method: "POST",
+        });
+        const { orderId } = await response.json();
+        return { orderId };
+      }}
+      /* TODO: Implement onApprove function */
+      onApprove={async ({ orderId }: OnApproveDataOneTimePayments) => {
+        await fetch(`/api/capture-order/${orderId}`, {
+          method: "POST",
+        });
+        console.log("Payment captured!");
+      }}
+    />
+  );
 };
